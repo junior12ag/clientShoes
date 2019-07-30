279a1d4908f254fbe50e1f224e5402a941c5fb34
Junior <junior12ag@gmail.com>
2019-07-30 19:33:25 -0400

    login producList

--- inventario/urls.py
+++ inventario/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('nuevoProducto', login_required(views.producto_view), name='nuevoProducto'),
-    path('', login_required(views.productoList_view), name='productoList'),
+    path('', views.productoList_view, name='productoList'),
     path('nuevaMarca', login_required(views.agregarMarca_view), name='nuevaMarca'),
     path('nuevaCategoria', login_required(views.agregarCategoria_view), name='nuevaCategoria'),
     path('editarProducto/<int:codigo>/', login_required(views.productoActualizar_view), name='editarProducto'),
